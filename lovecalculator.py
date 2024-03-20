df4c9d19463bb08afe73eb36df622d7417cba891
abrhaleyarefaine1997 <126599060+abrhaleyarefaine1997@users.noreply.github.com>
2024-03-20 01:56:20 +0100

    lovecalculatorwithcomment
    in this latest update comments are included.

--- lovecalculator.py
+++ lovecalculator.py
@@ -18,7 +18,7 @@ o = lower_combined_name.count("o")
 v = lower_combined_name.count("v")
 e = lower_combined_name.count("e")
 second_digit = l + o + v + e
-
+#score
 score = int(str(first_digit) + str(second_digit))
 
 if score < 10 or score > 90:
